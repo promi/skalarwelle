8a1a03be51aa0fd23edec2a0294d6e773acb7d87
Promi <prometheus@unterderbruecke.de>
2016-08-28 13:21:54 +0200

    Drop lib prefix for include dir, make #include relative

--- skalarwelle/src/skalarwelle-main-window.h
+++ skalarwelle/src/skalarwelle-main-window.h
@@ -23,7 +23,7 @@
 #include <glib-object.h>
 #include <gtk/gtk.h>
 
-#include <libskalarwelle-0/libskalarwelle.h>
+#include <libskalarwelle.h>
 #include "skalarwelle-connect-dialog.h"
 
 /* *INDENT-OFF* */
